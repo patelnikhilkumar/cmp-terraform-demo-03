039df8a2cd732a38abc1f677df1e6116e6232d2e
Nikhil Patel <nikhilkumar.pra.patel@hpe.com>
2025-10-30 10:49:54 -0400

    7th Commit

--- variable.tf
+++ variable.tf
@@ -2,46 +2,46 @@
 variable "subscription_id" {
   description = "Azure Subscription ID where the storage account will be created."
   type        = string
-  default     = "<%= customOptions.azurergprep_subscriptionid %>"
+  default     = "<%=customOptions.azurergprep_subscriptionid%>"
 }
 
 variable "tenant_id" {
   description = "Azure Active Directory Tenant ID."
   type        = string
-  default    = "<%= customOptions.azurergprep_tenantid %>"
+  default    = "<%=customOptions.azurergprep_tenantid%>"
 }
 
 variable "client_id" {
   description = "Azure Service Principal Client ID."
   type        = string
   sensitive   = true
-  default = "<%= customOptions.azurergprep_clientid %>"
+  default = "<%=customOptions.azurergprep_clientid%>"
 }
 
 variable "client_secret" {
   description = "Azure Service Principal Client Secret."
   type        = string
   sensitive   = true
-  default    = "<%= customOptions.azurergprep_clientsecret %>"
+  default    = "<%=customOptions.azurergprep_clientsecret%>"
 }
 
 # Resource configuration
 variable "resource_group_name" {
   description = "The name of the resource group."
   type        = string
-  default = "<%= customOptions.azurergprep_rg%>"
+  default = "<%=customOptions.azurergprep_rg%>"
 }
 
 variable "location" {
   description = "The Azure region where resources will be created."
   type        = string
-  default     = "<%= customOptions.azLocation %>"
+  default     = "<%=customOptions.azLocation%>"
 }
 
 variable "storage_account_name" {
   description = "The name of the storage account. Must be globally unique (3–24 lowercase letters and numbers)."
   type        = string
-  default     = "<%= customOptions.azSAName %>"
+  default     = "<%=customOptions.azSAName%>"
 }
 
 variable "account_tier" {
@@ -53,7 +53,7 @@ variable "account_tier" {
 variable "account_replication_type" {
   description = "Defines the replication type to use for this storage account (LRS, GRS, RAGRS, ZRS)."
   type        = string
-  default     = "<%= customOptions.azSAReplicationType %>"
+  default     = "<%=customOptions.azSAReplicationType%>"
 }
 
 variable "tags" {
